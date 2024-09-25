50f4cc5036764ddb0c4cb84b0a76135d59aa3d43
Reechka <R2echka@gmail.com>
2024-09-25 14:40:08 +0300

    variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend *= 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
+
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println(boxer1 + boxer2);
+        System.out.println(boxer2 - boxer1);
+
+        System.out.println(boxer2 % boxer1);
+
+        var workers = 640 / 8;
+        System.out.println("Всего работников в компании - " + workers + " человек");
+        workers += 94;
+        System.out.println("Если в компании работает " + workers + " человек, то всего " + (workers * 8) + " часов работы может быть поделено между сотрудниками");
+
     }
 }
