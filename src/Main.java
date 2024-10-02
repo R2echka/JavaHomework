3ba19ac9bf3c715756419641b2c6f33b0391625e
Reechka <R2echka@gmail.com>
2024-10-02 12:34:02 +0300

    cycles

--- src/Main.java
+++ src/Main.java
@@ -3,96 +3,58 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         // 1 задание
-        byte clientOS = 1;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке");
+        for (byte i = 0; i <= 10; i ++) {
+            System.out.println(i);
         }
 
         // 2 задание
-        short clientDeviceYear = 2015;
-        switch (clientOS) {
-            case 0:
-                if (clientDeviceYear >= 2015) {
-                    System.out.println("Установите версию приложения для iOS по ссылке");
-                } else {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                } 
-                break;
-            default:
-                if (clientDeviceYear >= 2015) {
-                    System.out.println("Установите версию приложения для Android по ссылке");
-                } else {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                }
+        for (byte i = 10; i >= 1; i --) {
+            System.out.println(i);
         }
 
         //3 задание
-        short year = 2024;
-        if (year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        } else if (year % 100 == 0) {
-            System.out.println(year + " год не является високосным");
-        } else if (year % 4 == 0) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
+        for (byte i = 0; i <= 17; i += 2) {
+            System.out.println(i);
         }
 
         // 4 задание
-        byte deliveryDistance = 95;
-        byte deliveryTime = 1;
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + deliveryTime);
-        } else if (deliveryDistance < 60) {
-            deliveryTime ++;
-            System.out.println("Потребуется дней: " + deliveryTime);
-        } else if (deliveryDistance < 100) {
-            deliveryTime += 2;
-            System.out.println("Потребуется дней: " + deliveryTime);
-        } else {
-            System.out.println("Так далеко доставки нет");
+        for (byte i = 10; i >= -10; i --) {
+            System.out.println(i);
         }
+
         // 5 задание
-        byte monthNumber = 12;
-        switch (monthNumber) {
-            case 1:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Зима");
-                break;
-            case 2:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Зима");
-                break;
-            case 3:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Весна");
-                break;
-            case 4:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Весна");
-                break;
-            case 5:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Весна");
-                break;
-            case 6:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Лето");
-                break;
-            case 7:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Лето");
-                break;
-            case 8:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Лето");
-                break;
-            case 9:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Осень");
-                break;
-            case 10:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Осень");
-                break;
-            case 11:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Осень");
-                break;
-            case 12:
-                System.out.println(monthNumber + "-й месяц принадлежит к сезону Зима");
-                break;
+        for (short i = 1904; i <= 2096; i += 4) {
+            System.out.println(i);
+        }
+
+        // 6 задание
+        for (byte i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
+
+        // 7 задание
+        for (short i = 1; i <= 512; i *= 2) {
+            System.out.println(i);
+        }
+
+        // 8 задание
+        int savings = 0;
+        for (byte i = 0; i <= 12; i ++) {
+            savings += 29000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
+        }
+
+        // 9 задание
+        savings = 0;
+        for (byte i = 0; i <= 12; i ++) {
+            savings *= 1.01;
+            savings += 29000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
+        }
+
+        // 10 задание
+        for (byte i = 1; i <= 10; i ++) {
+            System.out.println("2*" + i + "=" + i*2);
         }
     }
 }
