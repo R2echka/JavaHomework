cb34825f5c8963b1c39878392e4c7420408f509f
Reechka <R2echka@gmail.com>
2024-10-05 17:28:59 +0300

    cycles 2

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,79 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         // 1 задание
-        for (byte i = 0; i <= 10; i ++) {
-            System.out.println(i);
+        int savings = 0;
+        short month = 0;
+        while (savings <= 2459000) {
+            savings += 15000;
+            month ++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
         }
 
         // 2 задание
-        for (byte i = 10; i >= 1; i --) {
-            System.out.println(i);
+        System.out.println();
+        byte num = 0;
+        while (num < 10) {
+            num ++;
+            System.out.print(num + " ");
         }
+        System.out.println();
+        while (num >= 1) {
+            System.out.print(num + " ");
+            num --;
+        }
+        System.out.println();
 
         //3 задание
-        for (byte i = 0; i <= 17; i += 2) {
-            System.out.println(i);
+        System.out.println();
+        int people = 12_000_000;
+        for (int i = 1; i <= 10; i++) {
+            people += people / 1000 * (17-8);
+            System.out.println("Год " + i +", численность населения составляет " + people);
         }
 
         // 4 задание
-        for (byte i = 10; i >= -10; i --) {
-            System.out.println(i);
-        }
+        System.out.println();
+        int money = 15_000;
+        month = 0;
+        do { 
+            money *= 1.07;
+            month ++;
+            System.out.println("Месяц " + month + ", накоплено " + money);
+        } while (money < 12_000_000);
 
         // 5 задание
-        for (short i = 1904; i <= 2096; i += 4) {
-            System.out.println(i);
-        }
+        System.out.println();
+        money = 15_000;
+        month = 0;
+        do { 
+            money *= 1.07;
+            month ++;
+            if (month % 6 == 0) System.out.println("Месяц " + month + ", накоплено " + money);
+        } while (money < 12_000_000);
 
         // 6 задание
-        for (byte i = 7; i <= 98; i += 7) {
-            System.out.println(i);
+        System.out.println();
+        money = 15_000;
+        month = 0;
+        for (int i = 0; i <= 12 * 9; i++) {
+            money *= 1.07;
+            month ++;
+            if (month % 6 == 0) System.out.println("Месяц " + month + ", накоплено " + money);
         }
 
         // 7 задание
-        for (short i = 1; i <= 512; i *= 2) {
-            System.out.println(i);
+        System.out.println();
+        int friday = 3;
+        while (friday <= 31) {
+            System.out.println("Сегодня пятница, " + friday +"-е число. Необходимо подготовить отчет");
+            friday += 7;
         }
 
         // 8 задание
-        int savings = 0;
-        for (byte i = 0; i <= 12; i ++) {
-            savings += 29000;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
-        }
-
-        // 9 задание
-        savings = 0;
-        for (byte i = 0; i <= 12; i ++) {
-            savings *= 1.01;
-            savings += 29000;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
-        }
-
-        // 10 задание
-        for (byte i = 1; i <= 10; i ++) {
-            System.out.println("2*" + i + "=" + i*2);
+        System.out.println();
+        short year = 2024;
+        for (int i = year - 200; i <= year + 100; i++) {
+            if (i % 79 == 0) System.out.println(i);
         }
     }
 }
