cf0173df5b3bf016cc788045f69fa5866db1c22b
Reechka <R2echka@gmail.com>
2024-10-07 15:36:21 +0300

    fixes

--- src/Main.java
+++ src/Main.java
@@ -3,76 +3,81 @@ public class Main {
         // 1 задание
         int savings = 0;
         short month = 0;
+        final float procents = 1.01f;
+        final short monthlySavings = 15_000;
         while (savings <= 2459000) {
-            savings += 15000;
-            month ++;
+            savings *= procents;
+            savings += monthlySavings;
+            month += 1;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
         }
 
         // 2 задание
         System.out.println();
-        byte num = 0;
-        while (num < 10) {
-            num ++;
-            System.out.print(num + " ");
-        }
+        for (byte num = 0; num < 10; num += 1) System.out.print(num + " ");
         System.out.println();
-        while (num >= 1) {
-            System.out.print(num + " ");
-            num --;
-        }
+        for (byte num = 0; num >= 1; num -= 1) System.out.print(num + " ");
         System.out.println();
 
         //3 задание
         System.out.println();
-        int people = 12_000_000;
-        for (int i = 1; i <= 10; i++) {
-            people += people / 1000 * (17-8);
-            System.out.println("Год " + i +", численность населения составляет " + people);
+        final byte birthRate = 17;
+        final byte mortalityRate = 8;
+        int population = 12_000_000;
+        for (int i = 1; i <= 10; i += 1) {
+            population += population / 1000 * (birthRate-mortalityRate);
+            System.out.println("Год " + i +", численность населения составляет " + population);
         }
 
         // 4 задание
         System.out.println();
+        final float monthlyProcents = 1.07f;
         int money = 15_000;
         month = 0;
+        final int needToSave = 12_000_000;
         do { 
-            money *= 1.07;
-            month ++;
+            money *= monthlyProcents;
+            month += 1;
             System.out.println("Месяц " + month + ", накоплено " + money);
-        } while (money < 12_000_000);
+        } while (money < needToSave);
 
         // 5 задание
         System.out.println();
         money = 15_000;
         month = 0;
         do { 
-            money *= 1.07;
-            month ++;
+            money *= monthlyProcents;
+            month += 1;
             if (month % 6 == 0) System.out.println("Месяц " + month + ", накоплено " + money);
-        } while (money < 12_000_000);
+        } while (money < needToSave);
 
         // 6 задание
         System.out.println();
         money = 15_000;
         month = 0;
-        for (int i = 0; i <= 12 * 9; i++) {
-            money *= 1.07;
-            month ++;
+        final byte monthsInNineYears = 12 * 9;
+        for (int i = 0; i <= monthsInNineYears; i+= 1) {
+            money *= monthlyProcents;
+            month += 1;
             if (month % 6 == 0) System.out.println("Месяц " + month + ", накоплено " + money);
         }
 
         // 7 задание
         System.out.println();
         int friday = 3;
-        while (friday <= 31) {
+        final byte daysInMonth = 31;
+        final byte daysInWeek = 7;
+        while (friday <= daysInMonth) {
             System.out.println("Сегодня пятница, " + friday +"-е число. Необходимо подготовить отчет");
-            friday += 7;
+            friday += daysInWeek;
         }
 
         // 8 задание
         System.out.println();
         short year = 2024;
-        for (int i = year - 200; i <= year + 100; i++) {
+        int yearBefore = year - 200;
+        int yearAfter = year + 100;
+        for (int i = yearBefore; i <= yearAfter; i += 1) {
             if (i % 79 == 0) System.out.println(i);
         }
     }
