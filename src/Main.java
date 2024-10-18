8d14873b1f554ef89207a6a31b8f1b56913d1cc5
Reechka <R2echka@gmail.com>
2024-10-18 13:13:07 +0300

    objects and classes

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,12 @@
 
 public class Main {
-    // 1 задание
-    public static String leapYear(int year) {
-        if (year % 400 == 0) {
-            return year + " год - високосный";
-        } else if (year % 100 == 0) {
-            return year + " год - не високосный";
-        } else if (year % 4 == 0) {
-            return year + " год - високосный";
-        } else {
-            return year + " год - не високосный";
-        }
-    }
-
-    // 2 задание
-    public static String appVersion(int clientOS, int clientDeviceYear) {
-        // В задании написано про текущий год конечно, но допустим
-        int currentYear = 2015;
-        switch (clientOS) {
-            case 0 -> {
-                if (clientDeviceYear >= currentYear) {
-                    return "Установите версию приложения для iOS по ссылке";
-                } else {
-                    return "Установите облегченную версию приложения для iOS по ссылке";
-                }
-            }
-            default -> {
-                if (clientDeviceYear >= currentYear) {
-                    return "Установите версию приложения для Android по ссылке";
-                } else {
-                    return "Установите облегченную версию приложения для Android по ссылке";
-                }
-            }
-            }
-    }
-
-    // 3 задание
-    public static String delieveryTime(int deliveryDistance) {
-        int days = 1;
-        if (deliveryDistance < 20) {
-            return "Потребуется дней: " + days;
-        } else if (deliveryDistance < 60) {
-            days ++;
-            return "Потребуется дней: " + days;
-        } else if (deliveryDistance < 100) {
-            days += 2;
-            return "Потребуется дней: " + days;
-        } else {
-            return "Так далеко доставки нет";
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        // 1 задание
-        System.out.println(leapYear(2024));
-        System.out.println(leapYear(1342));
+        Author author1 = new Author("name1", "surname");
+        Author author2 = new Author("name2", "surname1");
 
-        // 2 задание
-        System.out.println();
-        System.out.println(appVersion(1, 2020));
-        System.out.println(appVersion(0, 2024));
+        Book book1 = new Book("book1", author2, 1348);
+        Book book2 = new Book("book2", author1, 2000);
 
-        // 3 задание
-        System.out.println();
-        System.out.println(delieveryTime(15));
-        System.out.println(delieveryTime(52));
-        System.out.println(delieveryTime(60));
-        System.out.println(delieveryTime(138));
+        book2.setYear(1582);
     }
 }
